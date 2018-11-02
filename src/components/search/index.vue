d4e46db09621f2b70fd065985b4e18ff88a1c55e
xerxh <xhxer512@163.com>
2018-11-02 19:37:53 +0800

    新的组件封装

---
+++ src/components/search/index.vue
@@ -0,0 +1,46 @@
+<template>
+  <div class="flex-row-start">
+    <div class="add-box">
+      <slot name="btnForm"/>
+    </div>
+    <slot />
+    <div class="searchInput">
+      <i class="el-icon-search search" />
+      <input
+        v-model="inputVal"
+        :placeholder="placeholder"
+        type="text"
+        @keyup.enter="search">
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Search',
+  components: {
+
+  },
+  props: {
+    placeholder: {
+      type: String,
+      default: ''
+    }
+  },
+  data() {
+    return {
+      inputVal: ''
+    }
+  },
+  methods: {
+    search() {
+      this.$emit('search', this.inputVal)
+    }
+  }
+}
+</script>
+
+<style rel="stylesheet/scss" scoped lang="scss">
+@import "src/styles/mixin.scss";
+
+</style>
