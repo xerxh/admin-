8155f7e5346ed8a2b970cbbb3f264d3a16e32a4f
xerxh <xhxer512@163.com>
2018-11-08 18:23:54 +0800

    完善了组件

--- src/components/layout/navList/index.vue
+++ src/components/layout/navList/index.vue
@@ -26,7 +26,9 @@ export default {
         { icon: 'el-icon-location', title: '学生管理', path: '/student/index' },
         { icon: 'el-icon-location', title: '老师管理', path: '/teacher/index' },
         { icon: 'el-icon-location', title: '商品管理', path: '/commodity/index' },
+        { icon: 'el-icon-location', title: '首页管理', path: '/index/index' },
         { icon: 'el-icon-location', title: '课程管理', path: '/course/index' },
+        { icon: 'el-icon-location', title: '订单管理', path: '/order/index' },
         { icon: 'el-icon-location', title: '运营管理', path: '/operation/index' }
       ]
     }
